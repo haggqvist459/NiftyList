022b54c287855912061012d647a52a7108f01d93
haggqvist459 <tim.haggqvist@gmail.com>
2022-06-27 18:08:13 +0200

    first commit

---
+++ NiftyList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NiftyList
+//
+//  Created by Tim Häggqvist Luotomäki on 23/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NiftyList/NiftyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiftyListApp.swift
+//  NiftyList
+//
+//  Created by Tim Häggqvist Luotomäki on 23/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct NiftyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
